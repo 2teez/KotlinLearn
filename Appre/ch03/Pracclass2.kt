062b34e74dd8a3c82a447d7adb512e12bafe39c8
timothy adigun <adigun.timothy@gmail.com>
2025-08-02 20:52:23 +0100

    added order to the class practice

--- Appre/ch03/Pracclass2.kt
+++ Appre/ch03/Pracclass2.kt
@@ -1,6 +1,6 @@
 package com.prac.pracclass2
 
-class TShirt(val size: String, val color: String, val price: Double, val image: String?) {
+class TShirt(val size: String, val color: String, val price: Double, val image: String? = null) {
     override fun toString() = "TShirt[size: $size, $color, $price, ${
     image ?: ""
   }]"
@@ -8,11 +8,27 @@ class TShirt(val size: String, val color: String, val price: Double, val image: 
 
 data class User(val name: String, val email: String, val chart: ShoppingChart)
 
-data class ShoppingChart(val item: MutableList<TShirt>)
+data class Address(val name: String, val city: String, val zip_code: String)
+
+data class ShoppingChart(val address: Address, val item: MutableList<TShirt>)
 
 fun main() {
     val ankara_tshit = TShirt("L", "blue", 2.1, "Lion-Head-Image")
     println(ankara_tshit)
     val polo_tshit = TShirt("L", "blue", 2.1, null)
     println(polo_tshit)
+
+    val john =
+            User(
+                    "john",
+                    "johnApp@gmail.com",
+                    ShoppingChart(
+                            Address("dundee", "angus", "dd2-2lp"),
+                            mutableListOf(
+                                    TShirt("XXL", "Grey", 3.21, "Leopard-figure"),
+                                    TShirt("XL", "black", 1.56)
+                            )
+                    )
+            )
+    println(john)
 }
