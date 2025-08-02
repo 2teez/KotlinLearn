ebcbc5664d56b1fd1e3fa0dcdc85cd5e91706737
timothy adigun <adigun.timothy@gmail.com>
2025-08-02 20:37:03 +0100

    include package on the stanalone kotlin file

--- Appre/ch03/Pracclass2.kt
+++ Appre/ch03/Pracclass2.kt
@@ -1,5 +1,18 @@
+package com.prac.pracclass2
+
+class TShirt(val size: String, val color: String, val price: Double, val image: String?) {
+    override fun toString() = "TShirt[size: $size, $color, $price, ${
+    image ?: ""
+  }]"
+}
+
+data class User(val name: String, val email: String, val chart: ShoppingChart)
+
+data class ShoppingChart(val item: MutableList<TShirt>)
 
 fun main() {
-    println("Hello, World!")
+    val ankara_tshit = TShirt("L", "blue", 2.1, "Lion-Head-Image")
+    println(ankara_tshit)
+    val polo_tshit = TShirt("L", "blue", 2.1, null)
+    println(polo_tshit)
 }
-    
