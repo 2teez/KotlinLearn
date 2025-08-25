84b31a226d06380f7385071d2267b2446721f1a4
timothy adigun <adigun.timothy@gmail.com>
2025-08-25 16:54:49 +0100

    practice infix in kotlin

--- practiced/Pracinfixfun.kt
+++ practiced/Pracinfixfun.kt
@@ -1,8 +1,19 @@
 package com.prac.pracinfixfun
 
-class Person(val name: String, val lastName: String) {
-    val fullName: String
+interface Humanable {
+    val name: String
+}
+
+class Person(override val name: String, val lastName: String) : Humanable {
+    var fullName: String
         get() = "$name, $lastName"
+        set(value) {
+            fullName += value
+        }
+    infix fun add(person: Person): Person {
+        this.fullName += person.fullName
+        return this
+    }
 }
 
 infix fun Person.remaned(name: String) = this.fullName.replace(this.name, name)
@@ -10,6 +21,15 @@ infix fun Person.remaned(name: String) = this.fullName.replace(this.name, name)
 fun main() {
     "Hello, World!".print()
     (Person("java", "jvm") remaned "JAVA").print()
+
+    var name = "clojure"
+    name += ", jvm"
+    name.print()
+
+    val names = mutableListOf("java", "rust", "zig-lang")
+    names += "python"
+    names += "perl"
+    names.print()
 }
 
 fun Any?.print() = println(this)
