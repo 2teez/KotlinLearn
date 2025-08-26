dd53cd9237f9826e2b5372feece5dbf28f3db04d
timothy adigun <adigun.timothy@gmail.com>
2025-08-26 09:51:46 +0100

    another look at object

--- practiced/PracData.kt
+++ practiced/PracData.kt
@@ -5,10 +5,22 @@ data class Person(var firstName: String, val lastName: String) {
         get() = "$firstName, $lastName"
 }
 
+data class Human(val name: String)
+
+interface User {
+    fun produce(): Human
+}
+
 fun main() {
     val p1 = Person("java", "jvm")
     val p2 = p1.copy()
     p2.firstName = "clojure"
     println("${p1.fullName}, ${p2.fullName}")
     println(p1 == p2)
+    //
+    val admin =
+            object {
+                fun produce(name: String) = Human(name)
+            }
+    println(admin.produce("kotlin"))
 }
