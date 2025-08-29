afa5cd0bf739386cfda298632e2835e76418fdb9
timothy adigun <adigun.timothy@gmail.com>
2025-08-29 11:09:13 +0100

    added other functions

--- practiced/Anon.kt
+++ practiced/Anon.kt
@@ -6,4 +6,23 @@ fun main() {
 
     val anSayHello = { println("Howdy World!") } // lambda expression
     anSayHello.invoke()
+    //
+    var fc = kounter()
+    var sc = kounter()
+    println(fc())
+    println(fc())
+    println(fc())
+    pp("second counter -> ") { println(sc()) }
+    pp("second counter -> ") { println(sc()) }
+    pp("first counter -> ") { println(fc()) }
+}
+
+fun kounter(count: Int = 0): () -> Int {
+    var lCount = count
+    return { lCount++ }
+}
+
+fun pp(msg: String = "", fn: () -> Unit) {
+    print(msg)
+    fn()
 }
